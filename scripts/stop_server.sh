fe92cd0388ddbc526c4952ff1606abee93430687
EC2 Default User <ec2-user@ip-172-31-14-181.us-east-2.compute.internal>
2025-03-22 05:10:06 +0000

    update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,9 @@
-sudo systemctll stop httpd.service  # Intentional error here
+#!/bin/bash
+# Add this at the top of your script to make it exit immediately if any command fails
+set -e
+
+# Your intentional error
+sudo systemctll stop httpd.service  # Misspelled command will now cause script to exit with non-zero status
+
+# To be extra sure, we can also add an explicit exit code
+exit 1
