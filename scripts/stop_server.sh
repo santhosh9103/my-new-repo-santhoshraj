ddc54e043864c7fdc3073547732b344bf1dcedc2
EC2 Default User <ec2-user@ip-172-31-14-181.us-east-2.compute.internal>
2025-03-22 04:47:48 +0000

    Intentionally adding error to demonstrate rollback

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-isExistApp="$(pgrep httpd)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop httpd.service
-fi
-isExistApp="$(pgrep tomcat)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop tomcat.service
-fi
-
+sudo systemctll stop httpd.service  # Intentional error here
